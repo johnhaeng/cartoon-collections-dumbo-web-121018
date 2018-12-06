42869e4270e20273cc4ebd923d6c607f3f04725d
Jun Haeng Lee <johnhaeng@gmail.com>
2018-12-06 08:22:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,15 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  index = 0
+  array.each_with_index {|name, index| puts "#{index + 1}. #{name}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  new_arr = []
+  
+  array.collect do |elements|
+    new_arr.push(elements.upcase)
+  end
+  return new_arr.join(' ')
 end
 
 def long_planeteer_calls# code an argument here
